33b5beadfe5f5cfb10f2ef5b23860bb29677c596
Christian John Guipo <cjohnguipo@gmail.com>
2022-12-22 12:22:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,8 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const port = process.env.APP_PORT || 3001; 
 
-const userRouter = require("./api/users/user.router");
-
-app.use(express.json());
-
-app.get("/api", (req, res)=> 
-    res.json({
-        success: 1,
-        message: "This is rest apis working."
-    })
-);
-
-app.use("/api/users", userRouter);
-
-app.listen(process.env.APP_PORT, () => {
-    console.log("Server is up and running on PORT : ", process.env.APP_PORT);
+app.listen(${port}, () => {
+    console.log("Server is up and running on PORT : ", ${port});
 });
