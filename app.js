e5f4d6328d2d41cd92a4c59df02c63341e2986be
Christian John Guipo <cjohnguipo@gmail.com>
2022-12-22 12:29:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const app = express();
 const port = process.env.APP_PORT || 3001; 
 
 app.listen(port, () => {
-    console.log("Server is up and running on PORT : ", ${port});
+    console.log(`Server is up and running on PORT : ${port}`);
 });
